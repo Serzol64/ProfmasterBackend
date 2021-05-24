bd0095000c390afc97b1ac689802753a443436c4
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-05-24 19:03:45 +0400

    Final

--- scripts/callback.js
+++ scripts/callback.js
@@ -1,80 +1,10 @@
-const lb = $('#callback-lightbox');
-
-const autoNotification = (type) => {
-    switch (type) {
-        case 0:
-            $('.backcall').removeClass('nf-close');
-
-            window.setTimeout(function () {
-                $('.backcall').addClass('nf-close');
-            },8000);
-        break;
-        case 1:
-            $('.feedback').removeClass('nf-close');
-
-            window.setTimeout(function () {
-                $('.feedback').addClass('nf-close');
-            },10000);
-        break;
-        
-    }
-}
 
 $(document).ready(function () {
     $('#rec308189190 .tn-elem[data-elem-id="1619273074425"]').click(CallBackOpen);
-    $('#callback-lightbox > .lb-h .lb_close').click(CallBackClose);
-    $('#callback-lightbox > .lb main form').submit(CBFSend);
 });
 
 const CallBackOpen = (e, t) => {
-    lb.removeClass("lb_closed");
-}
-const CallBackClose = (e, t) => {
-    lb.addClass("lb_closed");
-}
-const CBFSend = (e,t) => {
-    let res = null;
-    let points = [
-        $('#callback-lightbox > .lb-h .lb_close'),
-        $('#callback-lightbox > .lb main form div input, #callback-lightbox > .lb main form div button'),
-        $('#callback-lightbox > .lb main form div input')
-    ],
-        query = {phone: $('phone').val()};
-
-    console.log("Ready:" + $('#callback-lightbox > .lb main form').attr('method'));
-    console.log("Ready:" + $('#callback-lightbox > .lb main form').attr('action'));
-
-    $.ajax({
-        type: $('#callback-lightbox > .lb main form').attr('method'),
-        url: $('#callback-lightbox > .lb main form').attr('action'),
-        dataType: 'json',
-        data: query,
-        beforeSend: function () {
-            points[1].prop('disabled','');
-        },
-        success: function (data) {
-            res = JSON.parse(data);
-
-            $.each(res, function (i, data) { 
-                if (data.result == "OkSend") {
-                    autoNotification(0);
-                    points[0].trigger('click');
-                    points[2].val('');
-                    points[1].removeProp('disabled');
-                }
-            });
-            console.log("Result:" + data);
-        },
-        error: function (data) {
-            autoNotification(1);
-            points[0].trigger('click');
-            points[1].removeProp('disabled');
-
-            console.log("Error:" + data);
-        }
-    });
-
+    jivo_api.open({ start : 'call'});
     e.preventDefault();
-
 }
 
--- scripts/freeconsult.js
+++ scripts/freeconsult.js
@@ -7,39 +7,7 @@ const StartSend = (e, t) => {
     $('form#form308189190').trigger('submit');
 }
 const FCSend = (e,t) => {
-    let res = null;
-    const queryList = JSON.stringify({customer: $('.t-input').eq(0).val(), phone: $('.t-input').eq(1).val()});
-    let point = $('.t-input'),
-        query = {queryData: queryList};
-
-    console.log("Ready:" + $('form#form308189190').attr('method'));
-    $.ajax({
-        type: $('form#form308189190').attr('method'),
-        url: "http://f0544597.xsph.ru/aplex/profmasterLanding/freeconsultation.php",
-        dataType: 'json',
-        data: query,
-        beforeSend: function () {
-            point.prop('disabled','');
-        },
-        success: function (data) {
-            res = JSON.parse(data);
-
-            $.each(res, function (i, data) { 
-                if (data.result == "OkForm") {
-                    autoNotification(0);
-                    point.val('');
-                    point.removeProp('disabled');
-                }
-            });
-            console.log("Result:" + data);
-        },
-        error: function (data) {
-            autoNotification(1);
-            point.removeProp('disabled');
-
-            console.log("Error:" + data);
-        }
-    });
+    
 
     e.preventDefault();
 
